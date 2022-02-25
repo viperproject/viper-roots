7bc656b77c0b48d9a3f937594acc311927197511
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2022-02-25 16:49:32 +0100

    add if_Some methods to Util (taken from Benjamin Bonneau)

--- TotalUtil.thy
+++ TotalUtil.thy
@@ -31,4 +31,45 @@ abbreviation option_if :: "bool \<Rightarrow> 'a option \<Rightarrow> 'a option"
 abbreviation Some_if :: "bool \<Rightarrow> 'a \<Rightarrow> 'a option" where
   "Some_if b x \<equiv> option_if b (Some x)"
 
+subsection \<open>\<open>if_Some\<close>\<close>
+
+text \<open>interface for \<open>if_Some\<close> was initially defined by Benjamin Bonneau\<close>
+
+primrec if_Some :: "'a option \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> bool" where
+    "if_Some (Some x) f = f x"
+  | "if_Some None     f = True"
+
+lemma if_SomeI:
+  "(\<And>x. opt = Some x \<Longrightarrow> f x) \<Longrightarrow> if_Some opt f"
+  by (cases opt; simp)
+lemma if_SomeIex:
+  "opt = Some x \<Longrightarrow> f x \<Longrightarrow> if_Some opt f"
+  by (cases opt; simp)
+lemma if_SomeD:
+  "if_Some opt f \<Longrightarrow> opt = Some x \<Longrightarrow> f x"
+  by simp
+lemma if_Some_iff:
+  "(if_Some opt f) = (\<forall>x. opt = Some x \<longrightarrow> f x)"
+  by (cases opt; simp)
+
+lemma if_Some_split:
+  "P (if_Some opt f) = ((opt = None \<longrightarrow> P True) \<and> (\<forall>x. opt = Some x \<longrightarrow> P (f x)))"
+  by (cases opt; simp)
+lemma if_Some_split_asm:
+  "P (if_Some opt f) = (\<not> ((opt = None \<and> \<not> P True) \<or> (\<exists>x. opt = Some x \<and> \<not> P (f x))))"
+  by (cases opt; simp)
+
+lemma if_Some_imp:
+  "if_Some opt P \<Longrightarrow> (\<And>x. P x \<Longrightarrow> Q x) \<Longrightarrow> if_Some opt Q"
+  by (cases opt; simp)
+lemma if_Some_mono_strong:
+  "if_Some opt P \<Longrightarrow> (\<And>x. opt = Some x \<Longrightarrow> P x \<Longrightarrow> Q x) \<Longrightarrow> if_Some opt Q"
+  by (cases opt; simp)
+lemma if_Some_mono[mono]:
+  "P \<le> Q \<Longrightarrow> if_Some opt P \<le> if_Some opt Q"
+  by (cases opt; auto)
+lemma[fundef_cong]:
+  "x = y \<Longrightarrow> (\<And>z. y = Some z \<Longrightarrow> P z = Q z) \<Longrightarrow> if_Some x P = if_Some y Q"
+  by (cases y; simp)
+
 end
