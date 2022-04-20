00f552a3bd8c4f00c2bc30c654958b6b9e905301
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2022-04-20 23:15:40 +0200

    checkpoint syntactic expression relation

--- ViperBoogieBasicRel.thy
+++ ViperBoogieBasicRel.thy
@@ -15,8 +15,9 @@ type_synonym 'a bpl_mask_ty = "ref \<Rightarrow> 'a vb_field \<Rightarrow> real"
 record tr_vpr_bpl =
   heap_var :: Lang.vname
   mask_var :: Lang.vname
-(*  mask_read :: "expr \<Rightarrow> expr \<Rightarrow> expr"
-  mask_store :: "expr \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr" *)
+  mask_read :: "expr \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr" \<comment>\<open>arguments: heap, receiver, field\<close>
+  heap_read :: "expr \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr" \<comment>\<open>arguments: heap, receiver, field\<close>
+  field_translation :: "field_ident \<rightharpoonup> field_ident"
   fun_translation :: "ViperLang.function_ident \<rightharpoonup> Lang.fname"
   var_translation :: "ViperLang.var \<rightharpoonup> Lang.vname" \<comment>\<open>local Boogie variables\<close>
   lit_translation :: "ViperLang.lit \<Rightarrow> expr"
@@ -36,16 +37,16 @@ fun val_rel_vpr_bpl :: "'a vpr_val \<Rightarrow> 'a vbpl_val"
 
 subsection \<open>State relationship\<close>
 
-definition heap_rel :: "ViperLang.program \<Rightarrow> 'a total_heap \<Rightarrow> 'a bpl_heap_ty \<Rightarrow> bool"
-  where "heap_rel Pr h hb \<equiv> 
+definition heap_rel :: "ViperLang.program \<Rightarrow> (field_ident \<rightharpoonup> field_ident) \<Rightarrow> 'a total_heap \<Rightarrow> 'a bpl_heap_ty \<Rightarrow> bool"
+  where "heap_rel Pr tr_field h hb \<equiv> 
     \<forall> l :: heap_loc. if_Some 
-                       (\<lambda>field_vty. option_fold (\<lambda>res. val_rel_vpr_bpl (h l) = res) False (hb (Address (fst l)) (NormalField (snd l) field_vty)))
+                       (\<lambda>field_vty. option_fold (\<lambda>res. val_rel_vpr_bpl (h l) = res) False (hb (Address (fst l)) (NormalField (the (tr_field (snd l))) field_vty)))
                        (declared_fields Pr (snd l))"
 
-definition mask_rel :: "ViperLang.program \<Rightarrow> mask \<Rightarrow> 'a bpl_mask_ty \<Rightarrow> bool"
-  where "mask_rel Pr m mb \<equiv> 
+definition mask_rel :: "ViperLang.program \<Rightarrow> (field_ident \<rightharpoonup> field_ident) \<Rightarrow> mask \<Rightarrow> 'a bpl_mask_ty \<Rightarrow> bool"
+  where "mask_rel Pr tr_field m mb \<equiv> 
     \<forall> l :: heap_loc. if_Some 
-                       (\<lambda>field_vty. real_of_rat (Rep_prat (m l)) = (mb (Address (fst l)) (NormalField (snd l) field_vty)))
+                       (\<lambda>field_vty. real_of_rat (Rep_prat (m l)) = (mb (Address (fst l)) (NormalField (the (tr_field (snd l))) field_vty)))
                        (declared_fields Pr (snd l))"
 
 text \<open>\<^const>\<open>heap_rel\<close> and \<^const>\<open>mask_rel\<close> depend on the program, since the Viper type of a Viper field is required (currently)
@@ -65,9 +66,8 @@ definition state_rel_vpr_bpl :: "ViperLang.program \<Rightarrow> tr_vpr_bpl \<Ri
               (\<exists>val_vpr val_bpl. ((get_store_total \<omega>) var_vpr) = Some val_vpr \<and>
                                 ((local_state ns) var_vpr) = Some val_bpl \<and>
                                 val_rel_vpr_bpl val_vpr = val_bpl)) \<and>
-           (\<exists>hb. (global_state ns) (heap_var T) = Some (AbsV (AHeap hb)) \<and> heap_rel Pr (get_hh_total_full \<omega>) hb) \<and>
-           (\<exists>mb. (global_state ns) (mask_var T) = Some (AbsV (AMask mb)) \<and> mask_rel Pr (get_mh_total_full \<omega>) mb)"
-
+           (\<exists>hb. (global_state ns) (heap_var T) = Some (AbsV (AHeap hb)) \<and> heap_rel Pr (field_translation T) (get_hh_total_full \<omega>) hb) \<and>
+           (\<exists>mb. (global_state ns) (mask_var T) = Some (AbsV (AMask mb)) \<and> mask_rel Pr (field_translation T) (get_mh_total_full \<omega>) mb)"
 
 subsection\<open>function relation\<close>
 
@@ -75,16 +75,16 @@ subsection\<open>function relation\<close>
 abbreviation eval_heap_dep_bpl_fun :: "('a vbpl_absval) Semantics.fun_repr \<Rightarrow> ('a vbpl_val) list \<Rightarrow> 'a vbpl_val \<rightharpoonup> 'a vbpl_val"
   where "eval_heap_dep_bpl_fun f_bpl vs heap \<equiv> f_bpl [] (heap#vs)"
 
-definition fun_rel :: "ViperLang.program \<Rightarrow> 'a TotalExpressions.heapfun_repr \<Rightarrow> ('a vbpl_absval) Semantics.fun_repr \<Rightarrow> bool"
-  where "fun_rel Pr f_vpr f_bpl \<equiv> 
+definition fun_rel :: "ViperLang.program \<Rightarrow> (field_ident \<rightharpoonup> field_ident) \<Rightarrow> 'a TotalExpressions.heapfun_repr \<Rightarrow> ('a vbpl_absval) Semantics.fun_repr \<Rightarrow> bool"
+  where "fun_rel Pr tr_field f_vpr f_bpl \<equiv> 
            (\<forall>vs \<omega> v_vpr. f_vpr vs \<omega> = Some (Val v_vpr) \<longrightarrow>
-              (\<forall> h_bpl. heap_rel Pr (get_hh_total_full \<omega>) h_bpl \<longrightarrow>
+              (\<forall> h_bpl. heap_rel Pr tr_field (get_hh_total_full \<omega>) h_bpl \<longrightarrow>
                 has_Some (\<lambda>v_bpl. val_rel_vpr_bpl v_vpr = v_bpl) (eval_heap_dep_bpl_fun f_bpl ((map val_rel_vpr_bpl) vs) (AbsV (AHeap h_bpl)))))"
 
-definition fun_interp_rel :: "ViperLang.program \<Rightarrow> (ViperLang.function_ident \<Rightarrow> Lang.fname) \<Rightarrow> 'a TotalExpressions.interp \<Rightarrow> ('a vbpl_absval) Semantics.fun_interp \<Rightarrow> bool"
+definition fun_interp_rel :: "ViperLang.program \<Rightarrow> (field_ident \<rightharpoonup> field_ident) \<Rightarrow> (ViperLang.function_ident \<Rightarrow> Lang.fname) \<Rightarrow> 'a TotalExpressions.interp \<Rightarrow> ('a vbpl_absval) Semantics.fun_interp \<Rightarrow> bool"
   where 
-    "fun_interp_rel Pr f_tr \<Delta> \<Gamma> \<equiv> (\<forall>fid f_vpr. \<Delta> fid \<noteq> Some f_vpr \<longrightarrow>
-                                 has_Some (\<lambda>f_bpl. fun_rel Pr f_vpr f_bpl) (\<Gamma> (f_tr fid)))"
+    "fun_interp_rel Pr tr_field f_tr \<Delta> \<Gamma> \<equiv> (\<forall>fid f_vpr. \<Delta> fid \<noteq> Some f_vpr \<longrightarrow>
+                                 has_Some (\<lambda>f_bpl. fun_rel Pr tr_field f_vpr f_bpl) (\<Gamma> (f_tr fid)))"
 
 subsection \<open>expression relation\<close>
 
@@ -125,14 +125,56 @@ fun binop_rel :: "ViperLang.binop \<Rightarrow> Lang.binop"
   | "binop_rel ViperLang.And = Lang.And"
 (* TODO: PermDiv case *)
 
-inductive exp_rel :: "viper_expr \<Rightarrow> boogie_expr \<Rightarrow> bool"
+(*
+datatype pure_exp =
+  ELit lit
+  | Var var
+  | Unop unop pure_exp
+  | Binop pure_exp binop pure_exp
+  | CondExp pure_exp pure_exp pure_exp
+  | FieldAcc pure_exp field_ident
+  | Old label pure_exp
+
+  | Perm pure_exp field_ident
+  | PermPred predicate_ident "pure_exp list"
+
+  | FunApp function_ident "pure_exp list"
+  | Result (* Only for functions *)
+
+\<comment>\<open>unfolding P(l) in e\<close>
+  | Unfolding predicate_ident "pure_exp list" pure_exp
+
+  | Let pure_exp pure_exp
+  | PExists vtyp pure_exp
+  | is_pforall: PForall vtyp pure_exp
+*)
+
+inductive syn_exp_rel :: "tr_vpr_bpl \<Rightarrow> viper_expr \<Rightarrow> boogie_expr \<Rightarrow> bool"
+  for Tr :: tr_vpr_bpl
   where
-      "exp_rel (ViperLang.Var x) (Lang.Var y)"
-    | "exp_rel (ViperLang.ELit lit) (Lang.Lit litb)"
-    | " \<lbrakk> unop_rel uop = uopb; exp_rel e eb \<rbrakk> \<Longrightarrow>
-           exp_rel (ViperLang.Unop uop e) (Lang.UnOp uopb eb)"
-    | " \<lbrakk> binop_rel bop = bopb; exp_rel e1 e1b; exp_rel e2 e2b \<rbrakk> \<Longrightarrow>
-           exp_rel (ViperLang.Binop e1 bop e2) (Lang.BinOp e1b bopb e2b)"
-  
+    \<comment>\<open>The only global variables in the Boogie encoding are Heap and Mask, which do not have a variable 
+       counter part in Viper.\<close>
+       VarRel: 
+       "\<lbrakk> var_translation Tr x = Some y \<rbrakk> \<Longrightarrow>
+          syn_exp_rel Tr (ViperLang.Var x) (Lang.Var y)"
+     | LitRel: 
+       "\<lbrakk> lit_translation Tr lit = e_bpl \<rbrakk> \<Longrightarrow>
+          syn_exp_rel Tr (ViperLang.ELit lit) e_bpl"
+     | FieldAccRel:
+       "\<lbrakk> e_bpl = heap_read Tr (Lang.Var (heap_var Tr)) rcv_bpl f_bpl \<rbrakk> \<Longrightarrow>
+         syn_exp_rel Tr (FieldAcc e f) e_bpl"
+     | UnopRel:
+       "\<lbrakk> unop_rel uop = uopb; syn_exp_rel Tr e e_bpl \<rbrakk> \<Longrightarrow>
+           syn_exp_rel Tr (ViperLang.Unop uop e) (Lang.UnOp uopb e_bpl)"
+     | BinopRel:
+        "\<lbrakk> binop_rel bop = bopb; syn_exp_rel Tr e1 e1_bpl; syn_exp_rel Tr e2 e2_bpl \<rbrakk> \<Longrightarrow>
+           syn_exp_rel Tr (ViperLang.Binop e1 bop e2) (Lang.BinOp e1_bpl bopb e2_bpl)"
+     | FieldRel:
+        "syn_exp_rel Tr (Perm e f) e_bpl"
+     | FunAppRel:
+       "\<lbrakk> es_bpl = e_heap#e_args_bpl; 
+          list_all2 (\<lambda>e_vpr e_bpl. syn_exp_rel Tr e_vpr e_bpl) es e_args_bpl;
+          e_heap = Lang.Var (heap_var Tr) \<rbrakk> \<Longrightarrow>
+          syn_exp_rel Tr (FunApp f es) (FunExp f_bpl ts es_bpl)"
 
 end
