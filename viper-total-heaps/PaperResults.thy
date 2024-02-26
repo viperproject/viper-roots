418dfb7f6f45780fa6427baffa65495ac0ca41dc
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2024-02-26 15:14:25 +0100

    progress paper results doc up to Boogie procedure correctness

--- viper-total-heaps/PaperResults.thy
+++ viper-total-heaps/PaperResults.thy
@@ -1,5 +1,5 @@
 theory PaperResults
-imports ViperBoogieEndToEnd
+imports ViperBoogieEndToEnd StmtRelML
 begin
 
 text \<open>The following Isabelle theory file contains references to all the formalised results explicitly mentioned in
@@ -8,7 +8,7 @@ the paper. You can use the "Sidekick" view on the right side of the Isabelle IDE
 section or  subsection.
 
 In the Isabelle IDE, you can ctrl-and-click on the original definitions (highlighted in black everywhere
-except for constants where it has the same orange color as this text). You can use the green arrow buttons
+except for constants TODO where it has the same orange color as this text). You can use the green arrow buttons
 at the top of the Isabelle IDE to jump back (or forward) to the previous position after ctrl-clicking
 on a definition.
 
@@ -17,6 +17,8 @@ Clickable definitions in the Isabelle documentation (i.e., within text \<open>..
   \<^item> constants (for example, \<^const>\<open>red_stmt_total\<close>; note that here the clickable constant is in orange)
   \<^item> terms (for example, \<^term>\<open>red_stmt_total ctxt\<close>)
   \<^item> propositions (for example, \<^prop>\<open>red_stmt_total ctxt R \<Lambda> s \<sigma>\<^sub>v r\<^sub>v\<close>); these are just boolean terms
+  \<^item> Standard ML terms (for example, \<^ML>\<open>stmt_rel_tac\<close>) and types (for example, \<^ML_type>\<open>'a stmt_rel_hint\<close>);
+      we use Standard ML to define custom Isabelle tactics
 
 To refer to specific rules or proved theorems, we use Isabelle's \<open>lemmas\<close> keyword. 
 
@@ -31,7 +33,9 @@ lemmas example_for_a_proved_theorem = exhale_inhale_normal
 
 \<comment>\<open>You can ctrl-click on \<open>RedExhale\<close>, \<open>RedExhaleFailure\<close> and \<open>exhale_inhale_normal\<close>.
   You can also inspect the rules and lemmas by clicking anywhere right after the \<open>lemmas\<close> keyword
-  and then looking at the resulting Isabelle statement in the output panel.\<close>
+  and then looking at the resulting Isabelle statement in the output panel.
+  However, we recommend ctrl-clicking to get to the source, which often gives names to the premises
+  and is easier to read.\<close>
 
 
 text \<open>Whenever a ctrl-click takes you to another theory, you can use the green arrow buttons at the top 
@@ -105,7 +109,8 @@ text \<open>The expression evaluation is defined via \<^const>\<open>red_pure_ex
 
       As for the statement reduction, \<^term>\<open>R\<close> is not required for the Viper subset in the paper and is always
       instantiated as \<^term>\<open>\<lambda>_. True\<close> in our generated proofs. \<^term>\<open>ctxt\<close> is the same as for the statement reduction
-      but is redundant for the Viper subset in the paper and thus was omitted in the presentation.      
+      but is redundant for the expression evaluation for the Viper subset in the paper and thus was 
+      omitted in the presentation.      
 
       One difference to the paper, is that the expression evaluation in the formalisation takes two 
       Viper states as input instead of just one. Having two states is only required for Viper features
@@ -275,24 +280,75 @@ section \<open>4 Putting the Methodology to Work\<close>
 
 subsection \<open>4.1 State Relation\<close>
 
-text \<open>The state relation is defined in \<^const>\<open>state_rel0\<close>. The presented conjuncts in the paper are
-      represented as follows in the formalisation:
+text \<open>The state relation is defined in \<^const>\<open>state_rel0\<close>.
+      The notation \<open>R\<^sub>\<Gamma>\<^sub>b\<^sup>T\<^sup>r\<^sup>,\<^sup>A\<^sup>v((\<sigma>0\<^sub>v,\<sigma>\<^sub>v), \<sigma>\<^sub>b)\<close> in the paper corresponds to
+      \<^prop>\<open>state_rel0 Pr (\<lambda>_.True) A \<Lambda> TyRep Tr Av \<sigma>0\<^sub>v \<sigma>\<^sub>v ns\<close>
+      where \<open>\<Gamma>\<^sub>b\<close> captures both \<^term>\<open>A\<close> and \<^term>\<open>\<Lambda>\<close>, \<open>Pr\<close> is the Viper program representation and is left
+      implicit in the paper.
+
+      The presented conjuncts in the paper are represented as follows in the definition of \<^const>\<open>state_rel0\<close>:
       
       \<^item> State consistency: The conjunct \<open>consistent(\<sigma>\<^sub>v)\<close> corresponds to \<^term>\<open>wf_mask_simple (get_mh_total_full \<sigma>\<^sub>v)\<close>.
       \<^item> Field relation: The conjunct \<open>fieldRel\<^sub>\<Gamma>\<^sub>b(field(Tr), \<sigma>\<^sub>b)\<close> corresponds to
                         \<^term>\<open>field_rel Pr \<Lambda> (field_translation Tr) \<sigma>\<^sub>b\<close>.
+      \<^item> Auxiliary variable constraints: The conjunct \<open>\<forall>x,P. AV(x) = P \<longrightarrow> P(\<sigma>\<^sub>b(x))\<close> corresponds to
+                        \<^term>\<open>aux_vars_pred_sat \<Lambda> AuxPred \<sigma>\<^sub>b\<close>
       \<^item> Store relation: The conjunct \<open>stRel\<^sub>\<Gamma>\<^sub>b(var(Tr), \<sigma>\<^sub>v, \<sigma>\<^sub>b)\<close> corresponds to 
                         \<^term>\<open>store_rel A \<Lambda> (var_translation Tr) \<sigma>\<^sub>v \<sigma>\<^sub>b\<close>
       \<^item> Heap and permission mask relation: The conjunct \<open>hmRel\<^sub>\<Gamma>\<^sub>b(HM(Tr), \<sigma>\<^sub>v, \<sigma>\<^sub>b)\<close> corresponds to
-          \<^term>\<open> heap_var_rel Pr \<Lambda> TyRep (field_translation Tr) (heap_var Tr) \<sigma>\<^sub>v \<sigma>\<^sub>b \<and>
-                mask_var_rel Pr \<Lambda> TyRep (field_translation Tr) (mask_var Tr) \<sigma>\<^sub>v \<sigma>\<^sub>b\<close> 
+          \<^term>\<open>heap_var_rel Pr \<Lambda> TyRep (field_translation Tr) (heap_var Tr) \<sigma>\<^sub>v \<sigma>\<^sub>b \<and>
+               mask_var_rel Pr \<Lambda> TyRep (field_translation Tr) (mask_var Tr) \<sigma>\<^sub>v \<sigma>\<^sub>b\<close>
+          The conjunct \<open>hmRel\<^sub>\<Gamma>\<^sub>b(HM\<^sup>0(Tr), \<sigma>0\<^sub>v, \<sigma>\<^sub>b)\<close> corresponds to
+          \<^term>\<open>heap_var_rel Pr \<Lambda> TyRep (field_translation Tr) (heap_var_def Tr) \<sigma>0\<^sub>v \<sigma>\<^sub>b \<and>
+               mask_var_rel Pr \<Lambda> TyRep (field_translation Tr) (mask_var_def Tr) \<sigma>0\<^sub>v \<sigma>\<^sub>b\<close>
 \<close>
 
 subsection \<open>4.2 Non-Locality\<close>
 
+text \<open>The predicate \<open>Q\<^sub>p\<^sub>r\<^sub>e\<close> is defined in \<^const>\<open>framing_exh\<close>. 
+      The notation \<open>Q\<^sub>p\<^sub>r\<^sub>e(A, \<sigma>0\<^sub>v, \<sigma>\<^sub>v)\<close> corresponds to \<^prop>\<open>framing_exh ctxt_vpr (\<lambda>_.True) A \<sigma>0\<^sub>v \<sigma>\<^sub>v\<close>.
+      The extra parameter \<^term>\<open>ctxt_vpr\<close> (the Viper context) is not relevant for the subset presented 
+      in the paper in this case.
+
+      Lemma 4.1 is shown by:
+\<close>
+
+lemmas lemma_4_1 = exhale_inhale_normal
+
+text \<open>The parameter \<^term>\<open>StateCons\<close> is redundant for the subset presented in the paper and is always
+      instantiated to be \<^term>\<open>\<lambda>_.True\<close> in our proofs.
+      The premise \<^term>\<open>no_perm_assertion A \<and> no_unfolding_assertion A\<close> restricts the assertion \<^term>\<open>A\<close>
+      to be within the subset presented in the paper.
+
+      The premise \<open>\<not> \<langle>A, \<sigma>i\<^sub>v\<rangle> \<rightarrow>\<^sub>i\<^sub>n\<^sub>h F\<close> in the paper corresponds to
+      \<^prop>\<open>assertion_framing_state ctxt StateCons A \<omega>_inh\<close>.                 
+\<close>
+
 subsection \<open>4.3 Proof Automation\<close>
 
+text \<open>Our custom tactic to prove the forward simulation of Viper statements is defined in \<^ML>\<open>stmt_rel_tac\<close>
+      directly in Standard ML programming language, which is Isabelle's implementation language.
+     \<^ML>\<open>stmt_rel_tac\<close> invokes other tactics such as, for example, the simulation of 
+     \<open>remcheck\<close> operations defined in \<^ML>\<open>exhale_rel_aux_tac\<close>.
+
+     \<^ML>\<open>stmt_rel_tac\<close> is parametric in the hints and tactic for the primitive statements. The type 
+     signature makes this explicit via the argument types \<^ML_type>\<open>('a, 'i, 'e) stmt_rel_info\<close> (a record
+     that includes the primitive statement tactic) and \<^ML_type>\<open>'a stmt_rel_hint\<close> (for the hints).
+     In our proofs, we instantiate 'a to be \<^ML_type>\<open>atomic_rel_hint\<close> and define the tactic for the 
+     primitive statements to be \<^ML>\<open>atomic_rel_inst_tac\<close>.
+
+     Our tactics apply the rules proved for the different constructs. In some cases, we derive instantiated
+     versions of these rules upfront, which makes the tactics easier to write. For example, \<^ML>\<open>stmt_rel_tac\<close>
+     applies the lemma @{thm stmt_rel_seq_same_rel} for the sequential composition, which is the same as
+     the sequential composition rule SEQ-SIM shown in Figure 5, except that the input and output relation
+     are fixed to be the same.
+     \<comment>\<open>TODO: which parts of 4.3 should be reflected here?\<close>
+\<close>
+
 subsection \<open>4.4 Background Theory and Polymorphic Maps\<close>
+
+paragraph \<open>Boogie procedure correctness\<close>
+text \<open>test\<close>
 
 subsection \<open>4.5 Generating A Proof of the Final Theorem\<close>
 
