2b5e74e195d80d6b6fdf3ab4ce628fdcc403e971
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2024-03-05 11:54:03 +0100

    progress paper method correctness

--- viper-total-heaps/PaperResultsSupport.thy
+++ viper-total-heaps/PaperResultsSupport.thy
@@ -98,36 +98,38 @@ abbreviation states_differ_only_on_trace :: "'a full_total_state \<Rightarrow> '
   where "states_differ_only_on_trace \<omega>1 \<omega>2 \<equiv> get_store_total \<omega>1 = get_store_total \<omega>2 \<and> 
                                               get_total_full \<omega>1 = get_total_full \<omega>2"
 
-thm th_result_rel.cases
-
 lemma states_differ_trace_update_trace_eq:
   assumes "states_differ_only_on_trace \<omega>1 \<omega>2"
-  shows "update_trace_total \<omega>2 (get_trace_total \<omega>1) =  \<omega>1"
+  shows "update_trace_total \<omega>2 (get_trace_total \<omega>1) = \<omega>1"
+  apply (rule full_total_state.equality)
   using assms
-  sorry
+  by simp_all
 
+lemma states_differ_trace_update_trace_eq_2:
+  shows "states_differ_only_on_trace \<omega>1 (update_trace_total \<omega>1 t)"
+  by simp
  
 lemma exp_eval_inh_no_old_exp_trace_indep:
-  shows "ctxt, R, \<omega>_def1 \<turnstile> \<langle>e;\<omega>1\<rangle> [\<Down>]\<^sub>t resE \<Longrightarrow> 
+  shows "ctxt, (\<lambda>_. True), \<omega>_def1 \<turnstile> \<langle>e;\<omega>1\<rangle> [\<Down>]\<^sub>t resE \<Longrightarrow> 
         exp_in_paper_subset e \<Longrightarrow>
         states_differ_only_on_trace \<omega>1 \<omega>2 \<Longrightarrow> 
         \<omega>_def2 = None \<longleftrightarrow> \<omega>_def1 = None \<Longrightarrow> 
         (\<omega>_def2 \<noteq> None \<and> \<omega>_def1 \<noteq> None \<Longrightarrow> states_differ_only_on_trace (the \<omega>_def1) (the \<omega>_def2)) \<Longrightarrow>        
-         ctxt, R, \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t resE" and
-        "red_pure_exps_total ctxt R \<omega>_def1 es \<omega>1 resES \<Longrightarrow> 
+         ctxt, (\<lambda>_. True), \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t resE" and
+        "red_pure_exps_total ctxt (\<lambda>_. True) \<omega>_def1 es \<omega>1 resES \<Longrightarrow> 
          list_all (\<lambda>e. exp_in_paper_subset e) es \<Longrightarrow>
         states_differ_only_on_trace \<omega>1 \<omega>2 \<Longrightarrow> 
         \<omega>_def2 = None \<longleftrightarrow> \<omega>_def1 = None \<Longrightarrow> 
         (\<omega>_def2 \<noteq> None \<and> \<omega>_def1 \<noteq> None \<Longrightarrow> states_differ_only_on_trace (the \<omega>_def1) (the \<omega>_def2)) \<Longrightarrow>
-        red_pure_exps_total ctxt R \<omega>_def2 es \<omega>2 resES" and
-        "red_inhale ctxt R A \<omega>1 res1 \<Longrightarrow> 
+        red_pure_exps_total ctxt (\<lambda>_. True) \<omega>_def2 es \<omega>2 resES" and
+        "red_inhale ctxt (\<lambda>_. True) A \<omega>1 res1 \<Longrightarrow> 
               assertion_in_paper_subset A \<Longrightarrow>
               states_differ_only_on_trace \<omega>1 \<omega>2 \<Longrightarrow>
-              (res1 = RFailure \<longrightarrow> red_inhale ctxt R A \<omega>2 RFailure) \<and>
+              (res1 = RFailure \<longrightarrow> red_inhale ctxt (\<lambda>_. True) A \<omega>2 RFailure) \<and>
               (\<forall>\<omega>1'. res1 = RNormal \<omega>1' \<longrightarrow> 
-                     red_inhale ctxt R A \<omega>2 (RNormal (update_trace_total \<omega>1' (get_trace_total \<omega>2)))
+                     red_inhale ctxt (\<lambda>_. True) A \<omega>2 (RNormal (update_trace_total \<omega>1' (get_trace_total \<omega>2)))
               )" and
-        "unfold_rel ctxt R x12 x13 x14 x15 x16 \<Longrightarrow> True"
+        "unfold_rel ctxt (\<lambda>_. True) x12 x13 x14 x15 x16 \<Longrightarrow> True"
 proof (induction arbitrary: \<omega>2 \<omega>_def2 and \<omega>2 \<omega>_def2 and \<omega>2 rule: red_exp_inhale_unfold_inducts)
   case (RedLit \<omega>_def l uu)
   then show ?case 
@@ -186,7 +188,7 @@ next
   then show ?case by simp
 next
   case (RedField \<omega>_def e \<omega> a f v)
-  hence AddrEval: "ctxt, R, \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef (Address a))"
+  hence AddrEval: "ctxt, (\<lambda>_. True), \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef (Address a))"
     by simp
 
   show ?case
@@ -226,7 +228,7 @@ next
   qed
 next
   case (RedFieldNullFailure \<omega>_def e \<omega> f)
-  hence "ctxt, R, \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef Null)"
+  hence "ctxt, (\<lambda>_. True), \<omega>_def2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef Null)"
     by simp
   thus ?case  
     by (auto intro!: TotalExpressions.RedFieldNullFailure)    
@@ -247,7 +249,7 @@ next
   then show ?case by simp
 next
   case (RedSubFailure e' \<omega>_def \<omega>)
-  hence "red_pure_exps_total ctxt R \<omega>_def2 (sub_pure_exp_total e') \<omega>2 None"
+  hence "red_pure_exps_total ctxt (\<lambda>_. True) \<omega>_def2 (sub_pure_exp_total e') \<omega>2 None"
     using pure_exp_pred_subexp
     by presburger
   thus ?case 
@@ -270,9 +272,9 @@ next
     by metis
 next
   case (InhAcc \<omega> e_r r e_p p W' f res)  
-  hence RcvRed: "ctxt, R, Some \<omega>2 \<turnstile> \<langle>e_r;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef r)"
+  hence RcvRed: "ctxt, (\<lambda>_. True), Some \<omega>2 \<turnstile> \<langle>e_r;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VRef r)"
     by auto
-  moreover from InhAcc have PermRed: "ctxt, R, Some \<omega>2 \<turnstile> \<langle>e_p;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VPerm p)"
+  moreover from InhAcc have PermRed: "ctxt, (\<lambda>_. True), Some \<omega>2 \<turnstile> \<langle>e_p;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VPerm p)"
     by auto
 
   from \<open>th_result_rel _ _ _ _\<close>
@@ -285,7 +287,7 @@ next
       hence "res = RNormal \<omega>"
         using THResultNormal \<open>W' = _\<close>
         by simp
-      have RedInh2: "red_inhale ctxt R (Atomic (Acc e_r f (PureExp e_p))) \<omega>2 (RNormal \<omega>2)"
+      have RedInh2: "red_inhale ctxt (\<lambda>_. True) (Atomic (Acc e_r f (PureExp e_p))) \<omega>2 (RNormal \<omega>2)"
         apply (rule TotalExpressions.InhAcc[OF RcvRed PermRed])
         using \<open>r = Null\<close> THResultNormal
         by (auto intro: THResultNormal_alt)
@@ -299,27 +301,30 @@ next
         using THResultNormal inhale_perm_single_nonempty \<open>W' = _\<close>
         by fastforce
 
-      let ?W2' = "(if r = Null then {\<omega>2} else inhale_perm_single R \<omega>2 (the_address r,f) (Some (Abs_preal p)))"
-  
-      have "?W2' \<noteq> {}"
-        using THResultNormal \<open>W' = _\<close>
-        unfolding inhale_perm_single_def
-        sorry
-
+      let ?W2' = "(if r = Null then {\<omega>2} else inhale_perm_single (\<lambda>_. True) \<omega>2 (the_address r,f) (Some (Abs_preal p)))"
       let ?\<omega>2' = "update_mh_loc_total_full \<omega>2 (the_address r, f) (padd (get_mh_total_full \<omega>2 (the_address r, f)) (Abs_preal p))"
 
-      have "red_inhale ctxt R (Atomic (Acc e_r f (PureExp e_p))) \<omega>2 (RNormal ?\<omega>2')"
+      have "?\<omega>2' \<in> inhale_perm_single (\<lambda>_. True) \<omega>2 (the_address r, f) (Some (Abs_preal p))"
+        apply (rule inhale_perm_single_elem)
+        using \<open>\<omega>' \<in> W'\<close> \<open>W' = _\<close> \<open>r \<noteq> Null\<close> \<open>\<omega>' = _\<close> \<open>states_differ_only_on_trace \<omega> \<omega>2\<close>
+        unfolding inhale_perm_single_def 
+        by auto
+
+      have "red_inhale ctxt (\<lambda>_. True) (Atomic (Acc e_r f (PureExp e_p))) \<omega>2 (RNormal ?\<omega>2')"
         apply (rule TotalExpressions.InhAcc[where ?W' = ?W2',OF RcvRed PermRed])
          apply (rule HOL.refl)
         apply (rule THResultNormal_alt)
-        using inhale_perm_single_nonempty \<open>?W2' \<noteq> {}\<close> \<open>r \<noteq> Null\<close>
+        using inhale_perm_single_nonempty \<open>?\<omega>2' \<in> _\<close> \<open>r \<noteq> Null\<close>
           apply fastforce
-        using THResultNormal \<open>?W2' \<noteq> {}\<close>
+        using THResultNormal \<open>?\<omega>2' \<in> _\<close> 
         by auto
-
-      thus ?thesis
-        using \<open>\<omega>' = _\<close> \<open>res = _\<close> \<open>states_differ_only_on_trace \<omega> \<omega>2\<close>
-        sorry
+      moreover have "?\<omega>2' = (update_trace_total \<omega>' (get_trace_total \<omega>2))"
+        apply (simp add: \<open>\<omega>' = _\<close>)
+        apply (rule full_total_state.equality)
+        by (simp_all add:  \<open>states_differ_only_on_trace \<omega> \<omega>2\<close>)        
+      ultimately show ?thesis
+        using \<open>res = _\<close> \<open>states_differ_only_on_trace \<omega> \<omega>2\<close>
+        by simp        
       qed
   next
     case THResultMagic
@@ -341,19 +346,67 @@ next
   then show ?case by simp
 next
   case (InhPure \<omega> e b)
-  then show ?case sorry
+  hence "ctxt, (\<lambda>_. True), Some \<omega>2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VBool b)"
+    by simp
+  thus ?case
+    using TotalExpressions.InhPure
+    by (metis (full_types) InhPure.prems(2) states_differ_trace_update_trace_eq stmt_result_total.distinct(1) stmt_result_total.distinct(3) stmt_result_total.distinct(5) stmt_result_total.inject)    
 next
   case (InhStarNormal A \<omega> \<omega>'' B res)
-  then show ?case sorry
+  hence RedA: "red_inhale ctxt (\<lambda>_. True) A \<omega>2
+             (RNormal (update_trace_total \<omega>'' (get_trace_total \<omega>2)))"
+    by simp
+
+  show ?case
+  proof (cases res)
+    case RMagic
+    then show ?thesis 
+      by simp
+  next
+    case RFailure
+    then show ?thesis 
+      using RedA InhStarNormal TotalExpressions.InhStarNormal
+      by (metis assert_pred.elims(2) assert_pred_rec.simps(4) stmt_result_total.distinct(5) update_trace_total_hm_same update_trace_total_store_same)
+  next
+    case (RNormal \<omega>''')
+    then show ?thesis 
+      using RedA InhStarNormal TotalExpressions.InhStarNormal
+      by (metis (no_types, lifting) assert_pred.elims(2) assert_pred_rec.simps(4) inhale_only_changes_mask(3) update_trace_total_hm_same)     
+  qed        
 next
   case (InhStarFailureMagic A \<omega> resA B)
-  then show ?case sorry
+  then show ?case
+    using TotalExpressions.InhStarFailureMagic
+    by (metis assert_pred.simps assert_pred_rec.simps(4) stmt_result_total.distinct(3) stmt_result_total.distinct(5))
 next
   case (InhImpTrue \<omega> e A res)
-  then show ?case sorry
+  hence RedE: "ctxt, (\<lambda>_. True), Some \<omega>2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VBool True)"
+    by simp
+  show ?case 
+  proof (cases res)
+    case RFailure
+    hence *: "red_inhale ctxt (\<lambda>_. True) A \<omega>2 RFailure"
+      using InhImpTrue
+      by auto
+    thus ?thesis
+      using \<open>res = _\<close> TotalExpressions.InhImpTrue[OF RedE]
+      by auto
+  next
+    case (RNormal \<omega>')
+    hence *: "red_inhale ctxt (\<lambda>_. True) A \<omega>2 (RNormal (update_trace_total \<omega>' (get_trace_total \<omega>2)))"
+      using InhImpTrue
+      by auto
+    thus ?thesis
+      using \<open>res = _\<close> TotalExpressions.InhImpTrue[OF RedE]
+      by auto
+  qed (simp)        
 next
   case (InhImpFalse \<omega> e A)
-  then show ?case sorry
+  hence RedE: "ctxt, (\<lambda>_. True), Some \<omega>2 \<turnstile> \<langle>e;\<omega>2\<rangle> [\<Down>]\<^sub>t Val (VBool False)"
+    by simp
+  thus ?case
+    using TotalExpressions.InhImpFalse[OF RedE]
+    by (metis InhImpFalse.prems(2) states_differ_trace_update_trace_eq stmt_result_total.distinct(5) stmt_result_total.inject)
 next
   case (InhCondAssertTrue \<omega> e A res B)
   then show ?case sorry
